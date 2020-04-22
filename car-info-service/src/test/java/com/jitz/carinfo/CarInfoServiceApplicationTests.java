d5dbee95e527e9bf10d4868c9753ac4b14dd88a3
j.anand <jitesh.anand@bmigroup.com>
2020-04-22 18:48:39 +0100

    Initial Commits for the app

---
+++ car-info-service/src/test/java/com/jitz/carinfo/CarInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jitz.carinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
